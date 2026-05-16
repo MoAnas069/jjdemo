448e9a447786a15b57e008b0e4159cc8fb752960
ANAS AYYUB <moanasak069@gmail.com>
2026-05-16 20:23:06 +0530

    new heo

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@
 
 import { initLuxuryButton } from './LuxuryButton.js';
 import { initNavigation, initRevealAnimations, initSmoothScroll } from './nav.js';
-import { initHeroScroll } from './hero-scroll.js';
+import { initHeroLogoShatter } from './hero-logo-shatter.js';
 
 document.fonts.ready.then(() => {
   initNavigation();
   initRevealAnimations();
   initSmoothScroll();
   initTestimonialsSlider();
-  initHeroScroll();
+  initHeroLogoShatter();
 
   // Initialize luxury particle button on "Buy Your Home" CTA
   const buyBtn = document.getElementById('cta-buy');
